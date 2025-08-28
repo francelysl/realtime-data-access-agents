49211f3a4c6289385660a82e892d257324f1919f
francelysl <flomeli@hawk.iit.edu>
2025-08-27 19:19:45 -0500

    Added temporary placeholder to index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+	<main style={{ fontFamily: "system-ui, sans-serif", padding: 24 }}>
+	  <h1>Real-Time Data Access Platform</h1>
+	</main>
+  );
+}
